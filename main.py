12921e71ceb93af9578c73e99362bbf462786898
Megan Wilson <megan.wilson@sonarsource.com>
2023-11-27 16:48:05 -0600

    Eighth Commit or is it a miscount?

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import bravo
 import charlie
 from deltaFolder.delta import printing_myFileName
 import echoFolder.echo as echo
+import foxtrot
+import golf
 
 try:
     charlie.get_user_age()
     printing_myFileName() #delta
     print(__name__) # Script Mode
+    foxtrot.admit_failures()
 except ValueError:
     print("That's not a valid value for your age!")
 

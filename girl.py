a093f39f8846f8879dadbcc0071ef8b56f56340d
Megan Wilson <megan.wilson@sonarsource.com>
2023-11-27 17:17:55 -0600

    This is either Eleventh or Seventh Commmit who knows, who cares anymore?

---
+++ girl.py
@@ -0,0 +1,5 @@
+# My commentary on the (International) Radiotelephony Spelling Alphabet
+# I don't like using G as "golf"
+# We could go back "George" but the G has a "J" sound.
+# or change to the Gracie's Phonics song with "Girl"
+# Plus there isn't an equviant in the Greek

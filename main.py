b918dd33ecfe06257bff81ca59b80c6b90ae5a17
Megan Wilson <megan.wilson@sonarsource.com>
2023-11-27 16:20:16 -0600

    Fifth Commit with demo of Script vs Module Mode

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import alpha
 import beta
 import charlie
 from deltaFolder.delta import printing_myFileName
+import echoFolder.echo as echo
 
 try:
     charlie.get_user_age()
-    printing_myFileName()
+    printing_myFileName() #delta
+    print(__name__)
 except ValueError:
     print("That's not a valid value for your age!")
+
